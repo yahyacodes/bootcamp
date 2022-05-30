57b56adf38f762a151bc1d5c46270394e7a7ff82
yahyacodes <yahya.codin@gmail.com>
2022-05-30 09:21:20 +0300

    Initial commir

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoieWFoeWFjb2RlcyIsImEiOiJjbDNycmlpOG4wcDBtM2VtMWxzNWNzZmhmIn0.15vzFHGwWX5KHU_DcSHh3A';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.060982, 42.35725],
+  zomm: 18,
+});
